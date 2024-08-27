2e7d7f40c15a20d7f46d6fec407ea7fea0780546
Nelson Malbone <nhmalbone@icloud.com>
2024-08-27 18:13:57 -0400

    08/24/2024 Bro Code Email Slicer Program
    asking user for input of email
    then seperating from username and domain name
    in printing  out results

--- main.py
+++ main.py
@@ -2,3 +2,15 @@
 # Email Slicer Program
 # youtube link: https://youtu.be/4wGuB3oAKc4?si=p6WqFZXzKIxUXAEd&t=858
 
+# accepts user input
+email = input("Enter Your Email: ")
+
+# course of project is to split the email at the '@'
+index = email.index("@")
+# the index will return a number
+
+# create two new vars
+username = email[:index]
+domain = email[index:]
+
+print(f'Username is {username} and Domain is {domain}')
